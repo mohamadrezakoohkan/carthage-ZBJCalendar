c280a1792a73a394eb591ad1f238f594cc32ae75
mohamad reza koohkan <mohamad_koohkan@icloud.com>
2022-11-02 16:49:04 +0300

    Add a new target to build a ZBJCalendar framework

---
+++ ZBJCalendarLib/ZBJCalendarLib.h
@@ -0,0 +1,19 @@
+//
+//  ZBJCalendarLib.h
+//  ZBJCalendarLib
+//
+//  Created by Mohammad reza on 2.11.2022.
+//  Copyright © 2022 ZBJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZBJCalendarLib.
+FOUNDATION_EXPORT double ZBJCalendarLibVersionNumber;
+
+//! Project version string for ZBJCalendarLib.
+FOUNDATION_EXPORT const unsigned char ZBJCalendarLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZBJCalendarLib/PublicHeader.h>
+
+
